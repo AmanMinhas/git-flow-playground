6c0bd8e664d4444ad04aab99d395654b78b1227b
AmanMinhas <amandeepSinghMinhas@gmail.com>
2021-08-31 19:16:16 +0530

    Feature 5

--- file1.js
+++ file1.js
@@ -2,4 +2,5 @@ var line1 = "Hello my name is Suzie, with a zee";
 var line2 = "Dont mind me dancing with my friends!";
 console.log(line2);
 var line3 = "Ozzy Man Reviews are way to funny"
+var line4 = "When was the last time you ate something nice ?";
 console.log(line3)
