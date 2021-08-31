05e5036bd9d09c49b3ee8bff1000b358c2eab435
AmanMinhas <amandeepSinghMinhas@gmail.com>
2021-08-31 19:06:21 +0530

    hotfix 2

--- file1.js
+++ file1.js
@@ -1,4 +1,5 @@
 var line1 = "Hello my name is Suzie, with a zee";
 var line2 = "Dont mind me dancing with my friends!";
 console.log(line2);
-var line3 = "Ozzy Man Reviews are funny"
+var line3 = "Ozzy Man Reviews are way to funny"
+console.log(line3)
