39678b255f8076be2486b199b7a853cfdbac196b
AmanMinhas <amandeepSinghMinhas@gmail.com>
2021-09-02 13:09:07 +0530

    hotfix-6

--- file2.js
+++ file2.js
@@ -3,3 +3,4 @@ var line2 = "Lays >>>>> Uncle Chips";
 var line3 = "Can a movie be fun even if a crime didn't happen in it ?";
 var line4 = "It's Always Sunny in Philadelphia is hilarious";
 var line5 = "Dobo is a good Dog.";
+console.log(line5);
