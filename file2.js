b566a3d308e83f4c500ca3a8623bd57ce2441763
AmanMinhas <amandeepSinghMinhas@gmail.com>
2021-09-08 14:55:02 +0530

    Aleks Feature branch

--- file2.js
+++ file2.js
@@ -4,7 +4,7 @@ var line3 = "Can a movie be fun even if a crime didn't happen in it ?";
 var line4 = "It's Always Sunny in Philadelphia is hilarious";
 var line5 = "Dobo is a good Dog.";
 console.log(line5);
-var line6 = "You learn something new everyday";
+var line6 = "This is a change suggested by Alek";
 console.log(line6);
 var line7 = "It is what it is";
 var line8 = "Forest Life";
